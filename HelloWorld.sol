7e6225644ff37dd6655e8b12281145710b4dae03
RafaMechis <144859564+rmechis@users.noreply.github.com>
2024-05-02 09:31:17 -0300

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,5 +3,22 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract HelloWorld {
-  string public message = "Hello, world!";
+  
+  string public message;
+  address private owner;
+
+  constructor() {
+    message = "Hello, world!";
+    owner = msg.sender;
+  }
+
+  function displayMessage() public view returns(string memory) {
+    return message;
+  }
+
+  function changeMessage(string memory newMessage) public {
+    if (owner == msg.sender) { 
+      message = newMessage;
+    }
+  }
 }
